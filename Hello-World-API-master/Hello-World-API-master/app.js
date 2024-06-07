99078e613882b16a9d098b57ad8cb5721382978d
Emresen54 <142902798+Emresen54@users.noreply.github.com>
2024-06-07 10:49:46 +0200

    Add files via upload

---
+++ Hello-World-API-master/Hello-World-API-master/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+// Serve the frontend files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// API route
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'Hallo Welt' });
+});
+
+app.get('/api/hello2', (req, res) => {
+  res.json({ message: 'Emre' });
+});
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
